3a30ef204a6b5d5344b551e9bc9b03d14b86ae57
Abdullakhujaev Bekhruz <a00913965@china.huawei.com>
2026-01-04 18:14:19 +0500

    turned folders into packages

--- sample.py
+++ sample.py
@@ -3,4 +3,8 @@ def get_trigger_name(line_string):
     list = line_string.split(',')
     return list[1]
     
-print(get_trigger_name('t1,TITLE,election'))
+def main():
+    print(get_trigger_name('t1,TITLE,election'))
+
+if (__name__ == "__main__"):
+    main()
